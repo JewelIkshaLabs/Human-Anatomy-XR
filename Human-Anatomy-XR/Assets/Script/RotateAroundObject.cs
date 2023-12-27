f67f3919a63035f1807f61bc85a6ab0fed01034a
JewelIkshaLabs <jewel@ikshalabs.com>
2023-12-27 12:09:29 +0530

    Added some Models

---
+++ Human-Anatomy-XR/Assets/Script/RotateAroundObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundObject : MonoBehaviour
+{
+    [SerializeField] Transform _kneeTransform;
+    [SerializeField] float _rotationSpeed;
+
+    void Update()
+    {
+        RotateAroundAny(_kneeTransform);
+    }
+
+    void RotateAroundAny(Transform target)
+    {
+        transform.RotateAround(target.position, Vector3.up, _rotationSpeed);
+    }
+}
