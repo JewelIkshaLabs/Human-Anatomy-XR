21df565bbcce5772adc99a9e7d5a3b0a2dd420d5
apeksha-iksha <apeksha@ikshalabs.com>
2024-01-08 12:18:59 +0530

    added script to make application responsive

---
+++ Human-Anatomy-XR/Assets/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameObject;
+    private CanvasScaler canvasScaler;
+    void Start()
+    {
+        canvasScaler = gameObject.GetComponent<CanvasScaler>();
+        Resolution[] resolutions = Screen.resolutions;
+        foreach (var res in resolutions)
+        {
+            Debug.Log(res.width + "x" + res.height + " : " + res.refreshRateRatio);
+           
+        }
+
+        canvasScaler.referenceResolution = new Vector2(resolutions[0].width, resolutions[0].height );
+        
+    }
+}
